7c95380f645af56145226162c49b59db0e786006
isabel <isabel@isabelroses.com>
2024-05-11 18:51:10 +0100

    fix: deps

--- default.nix
+++ default.nix
@@ -1,8 +1,31 @@
-{rustPlatform}:
-rustPlatform.buildRustPackage {
-  pname = "ringfairy";
-  version = "0.1.2";
+{
+  lib,
+  darwin,
+  stdenv,
+  openssl,
+  pkg-config,
+  rustPlatform,
+}: let
+  inherit (darwin.apple_sdk.frameworks) Security CoreFoundation SystemConfiguration;
+in
+  rustPlatform.buildRustPackage {
+    pname = "ringfairy";
+    version = "0.1.2";
 
-  src = ./.;
-  cargoLock.lockFile = ./Cargo.lock;
-}
+    src = ./.;
+    cargoLock.lockFile = ./Cargo.lock;
+
+    buildInputs =
+      [
+        openssl
+      ]
+      ++ lib.optionals stdenv.isDarwin [
+        Security
+        CoreFoundation
+        SystemConfiguration
+      ];
+
+    nativeBuildInputs = [
+      pkg-config
+    ];
+  }
